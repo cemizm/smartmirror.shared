baccd92ea864ca4c9b330ea687f63264e9a36530
Cem Basoglu <cem.basoglu@web.de>
2017-07-05 17:46:19 +0200

    added widget settings

--- src/models/widget.ts
+++ src/models/widget.ts
@@ -2,12 +2,100 @@
  * Created by cem on 09.06.17.
  */
 
-import {WidgetType, WidgetSide} from './enums';
+import {WidgetType, WidgetSide} from "./enums";
 
 export interface Widget {
-    name: string;           // Name of the Widget
-    type: WidgetType;       // The Widget Type
-    side: WidgetSide;       // The Side of the Mirror to Display the Widget
-    order: number;          // The Order Position inside the Mirror Side
-    setting: any;           // Settings of the Widget
+    /**
+     * Name of the widget
+     */
+    name: string;
+    /**
+     * Type of the Widget
+     */
+    type: WidgetType;
+    /**
+     * The Side of the Mirror to Display the Widget
+     */
+    side: WidgetSide;
+    /**
+     * The Order Position inside the Mirror Side
+     */
+    order: number;
+    /**
+     * Settings of the Widget
+     */
+    setting: WeatherSetting | TaskSetting | NewsSetting | MailSettings | CalendarSettings;
+}
+
+/**
+ * Settings of the Weather Widget
+ */
+export interface WeatherSetting {
+    /**
+     * The City for Weather retrieval
+     */
+    city: string;
+}
+
+/**
+ * Setting for the Task Widget
+ */
+export interface TaskSetting {
+
+    /**
+     * Maximal count of task items to display
+     */
+    maxCount: number;
+    /**
+     * OAuth token for google tasks
+     */
+    oAuthToken: string;
+}
+
+/**
+ * Setting for the News Widget
+ */
+export interface NewsSetting {
+    /**
+     * Url of the RSS News Feed
+     */
+    feedUrl: string;
+
+    /**
+     * Maximal count of news items to display
+     */
+    maxCount: number;
+}
+
+export interface MailSettings {
+
+    /**
+     * Maximal count of email items to display
+     */
+    maxCount: number;
+
+    /**
+     * show only unread email
+     */
+    unread: boolean;
+
+    /**
+     * OAuth token for google mail
+     */
+    oAuthToken: string;
+
+}
+
+export interface CalendarSettings {
+
+    /**
+     * Maximal count of email items to display
+     */
+    maxCount: number;
+
+    /**
+     * OAuth token for google mail
+     */
+    oAuthToken: string;
+
 }
